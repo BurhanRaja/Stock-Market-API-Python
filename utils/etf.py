e8f6355c66a2ff16b8fba5798227ef5383203e86
BurhanRaja <burhanraja02@yahoo.com>
2023-06-27 10:10:04 +0530

    best etf added

--- utils/etf.py
+++ utils/etf.py
@@ -4,18 +4,21 @@ from yahooquery import Ticker
 import yfinance as yf
 import pandas as pd
 
-def all_etfs(offset: int = 1, limit: int = 10):
+refinedArray = []
+
+def all_etfs():
     tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_Indian_exchange-traded_funds', match="Name")
     data = tickers[0].to_json(orient="records")
     objArr = json.loads(data)
 
     dataArr = []
 
-    for ticker in objArr[offset:limit]:
+    for ticker in objArr[1:]:
         dataArr.append({
             "name": ticker['0'],
             "symbol": ticker['1']
         })
+    
     return dataArr
     
 def singleETFs(symbol: str):
